851476cacb7dd4ad2cf2d3e3ae8e86050f9883a7
iwyg <mail@thomas-appel.com>
2016-03-31 22:59:35 +0200

    update

--- public/src/runtime/constants.js
+++ public/src/runtime/constants.js
@@ -3,13 +3,13 @@
 export const CONTEXT = window;
 export const DEFAULT_URL = CONTEXT.DEFAULT_URL;
 export const JMG_CONFIG  = CONTEXT.JMG_CONFIG;
-export const ENV  = (function() {
-  let proc = require('process');
-  if (proc.env && proc.env.NODE_ENV !== undefined) {
-    return proc.env.NODE_ENV;
+export const ENV  = (function(undefined) {
+  if (process !== undefined && process.env !== undefined && process.env.NODE_ENV !== undefined) {
+    return process.env.NODE_ENV;
   }
 
-  return 'development';
+  return NODE_ENV !== undefined ? NODE_ENV : 'development';
 }());
+
 
 export const PRODUCTION = 'production';
